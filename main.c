f00e7520867d86a4c266e727f3c5054e0848cc68
jklmnn <jklmnn@web.de>
2014-11-11 15:41:29 +0100

    Fixed some crash reasons.

--- main.c
+++ main.c
@@ -21,11 +21,16 @@ int getlen(char* str){
 	return i;
 }
 
+void _help(char *name){
+	printf("ImageJs Version %d.%d.%d\n", VERSION_MAJ, VERSION_MIN, VERSION_FIX);
+	printf("Usage: %s [option] [javascript file]\n", name);
+	printf("Options:  gif, bmp, webp\n");
+	}
+
+
 int main(int argc, char *argv[]){
 	if(argc < 3){
-		printf("ImageJs Version %d.%d.%d\n", VERSION_MAJ, VERSION_MIN, VERSION_FIX);
-		printf("Usage: %s [option] [javascript file]\n", argv[0]);
-		printf("Options:  gif, bmp, webp\n");
+		_help(argv[0]);
 		return 1;
 	}
 	FILE *in;
@@ -33,12 +38,17 @@ int main(int argc, char *argv[]){
 	int filesize;
 	char *buf;
 	in = fopen(argv[2], "rb");
-	fseek(in, 0, SEEK_END);
-	filesize = ftell(in);
-	rewind(in);
-	buf = (char*)malloc(filesize * sizeof(char));
-	fread(buf, 1, filesize, in);
-	fclose(in);
+	if(in){
+		fseek(in, 0, SEEK_END);
+		filesize = ftell(in);
+		rewind(in);
+		buf = (char*)malloc(filesize * sizeof(char));
+		fread(buf, 1, filesize, in);
+		fclose(in);
+	}else{
+		printf("File %s could not be opened!\n", argv[2]);
+		return 2;
+	}
 	char *outbuf;
 	if(strcmp(argv[1], "bmp") == 0){
 		outbuf = bmp_js(buf, filesize);
@@ -46,20 +56,22 @@ int main(int argc, char *argv[]){
 		for(int i = 0; i < filesize + BMP_JS_HEADER; i++){
 			fprintf(out, "%c", outbuf[i]);
 		}
-	}
-	if(strcmp(argv[1], "gif") == 0){
+	}else if(strcmp(argv[1], "gif") == 0){
 		outbuf = gif_js(buf, filesize);
 		out = fopen(gif_filename(argv[2], getlen(argv[2])), "wb");
 		for(int i = 0; i < filesize + GIF_JS_HEADER; i++){
 			fprintf(out, "%c", outbuf[i]);
 		}
-	}
-	if(strcmp(argv[1], "webp") == 0){
+	}else if(strcmp(argv[1], "webp") == 0){
 		outbuf = webp_js(buf, filesize);
 		out = fopen(webp_filename(argv[2], getlen(argv[2])), "wb");
 		for(int i = 0; i < filesize + WEBP_JS_HEADER; i++){
 			fprintf(out, "%c", outbuf[i]);
 		}
+	}else{
+		_help(argv[0]);
+		free(buf);
+		return 1;
 	}
 	fclose(out);
 	free(buf);
