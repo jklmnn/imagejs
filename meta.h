b6b8464dff5880aec2d230a15dac3a564f2b2674
jklmnn <jklmnn@web.de>
2014-11-12 12:32:16 +0100

    Added version to 0.3.1

--- meta.h
+++ meta.h
@@ -9,7 +9,7 @@ License: GNU GENERAL PUBLIC LICENSE Version 3
 
 #define VERSION_MAJ 0
 #define VERSION_MIN 3
-#define VERSION_FIX 0
+#define VERSION_FIX 1
 #define AUTHOR "jklmnn"
 #define HOMEPAGE "http://jklmnn.de/imagejs"
 #define LICENSE "GPLv3"
